7a5441ee0ae7ba311c7ca840d1891480b4c16bfd
onlyones112 <op9776943@gmail.com>
2026-09-21 16:44:32 +0530

    Add files via upload

---
+++ src/commands/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const { Client, GatewayIntentBits, Collection, Partials } = require('discord.js');
+const { loadCommands } = require('./utils/commandLoader');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildModeration,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+  ],
+  partials: [Partials.Channel, Partials.Message, Partials.Reaction, Partials.User, Partials.GuildMember],
+});
+
+client.commands = new Collection();
+loadCommands(client, path.join(__dirname, 'commands'));
+
+const eventsDir = path.join(__dirname, 'events');
+for (const file of fs.readdirSync(eventsDir).filter((f) => f.endsWith('.js'))) {
+  const event = require(path.join(eventsDir, file));
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+client.login(process.env.DISCORD_TOKEN);
